607540e217e4585d93031829bdeb6bb7d16c6196
msy7822-ux <msy780022@gmail.com>
2023-05-02 05:03:17 +0900

    remove nav😄

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,14 +4,14 @@ import { ReactNode } from "react";
 const Layout = ({ children }: Props) => {
   return (
     <div>
-      <nav className="flex flex-rows p-2 bg-red-500 text-white font-bold">
+      {/* <nav className="flex flex-rows p-2 bg-red-500 text-white font-bold">
         <Link href="/">
           <span>Home</span>
         </Link>
         <Link href="/about">
           <span>About</span>
         </Link>
-      </nav>
+      </nav> */}
       {children}
     </div>
   );
