84eb03adc671067207af1a0901744462863eede4
msy7822-ux <msy780022@gmail.com>
2023-05-02 06:31:47 +0900

    getStaticProps削除を試す

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,32 @@
 import type { NextPage, GetStaticProps } from "next";
 import Image from "next/image";
 
-export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pokemon`);
-  const response = await res.json();
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pokemon`);
+//   const response = await res.json();
 
-  return {
-    props: {
-      response,
-    },
-    revalidate: 10,
-  };
-};
+//   return {
+//     props: {
+//       response,
+//     },
+//     revalidate: 10,
+//   };
+// };
 
-type Props = {
-  response: {
-    img: string;
-    timestamp: string;
-  };
-};
+// type Props = {
+//   response: {
+//     img: string;
+//     timestamp: string;
+//   };
+// };
 
-const Home: NextPage<Props> = ({ response }) => {
+// const Home: NextPage<Props> = ({ response }) => {
+const Home: NextPage = () => {
   return (
     <div>
-      <div>timestamp: {response.timestamp}</div>
-      <Image src={response.img} alt="pokemon" width={200} height={200} />
+      a
+      {/* <div>timestamp: {response.timestamp}</div>
+      <Image src={response.img} alt="pokemon" width={200} height={200} /> */}
     </div>
   );
 };
