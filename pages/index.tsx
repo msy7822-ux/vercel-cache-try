bde2e67291690ee4a48b95741c7121f5b0cd4691
msy7822-ux <msy780022@gmail.com>
2023-05-11 10:44:04 +0900

    revalidate modified🚀

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,30 @@
 import type { NextPage, GetStaticProps } from "next";
 import Image from "next/image";
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pokemon`);
-//   const response = await res.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pokemon`);
+  const response = await res.json();
 
-//   return {
-//     props: {
-//       response,
-//     },
-//     revalidate: 10,
-//   };
-// };
+  return {
+    props: {
+      response,
+    },
+    revalidate: 20,
+  };
+};
 
-// type Props = {
-//   response: {
-//     img: string;
-//     timestamp: string;
-//   };
-// };
+type Props = {
+  response: {
+    img: string;
+    timestamp: string;
+  };
+};
 
-// const Home: NextPage<Props> = ({ response }) => {
-const Home: NextPage = () => {
+const Home: NextPage<Props> = ({ response }) => {
   return (
     <div>
-      a
-      {/* <div>timestamp: {response.timestamp}</div>
-      <Image src={response.img} alt="pokemon" width={200} height={200} /> */}
+      <div>timestamp: {response.timestamp}</div>
+      <Image src={response.img} alt="pokemon" width={200} height={200} />
     </div>
   );
 };
